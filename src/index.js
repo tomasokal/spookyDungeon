a6277272423fadfd6c19fd3097b38bc4c08150f8
Tomas Okal <tomas.okal@gmail.com>
2023-01-13 23:06:27 -0600

    Add basic UI crosshairs.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { Suspense } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
 import Game from './Game.js'
+import UI from './utilities/UI'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
     <Suspense fallback={null}>
         <Game />
+        <UI />
     </Suspense>  
 )
